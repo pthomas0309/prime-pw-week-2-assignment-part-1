1f9de474b8c32f728614429ec0c9798206fa4d96
pthomas0309 <preston.thomas355@gmail.com>
2021-04-10 15:09:12 -0500

    set adventurous variable to true and fixed lastName

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,7 +2,7 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'preston';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName = 'thomas'
+let lastName = 'thomas';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + ' ' + lastName;
@@ -15,7 +15,7 @@ let luckyNumber = 7;
 // Refer back to the videos if you need help with this one.
 console.log( 'My name is ' + fullName + ', and I think ' + luckyNumber + ' is a winner!' );
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
