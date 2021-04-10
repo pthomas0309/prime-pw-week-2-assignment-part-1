be990a67d3726e648d72b65fff1b18a8fea2bf9e
pthomas0309 <preston.thomas355@gmail.com>
2021-04-10 18:25:56 -0500

    added two pets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -21,9 +21,9 @@ let food = 'Chicken Couscous';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 1;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 1
+let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
-
+pets = pets + 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
