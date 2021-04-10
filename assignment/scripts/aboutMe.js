f5b52eaa4e621d6c8126243fd9f79871c20ee645
pthomas0309 <preston.thomas355@gmail.com>
2021-04-10 14:58:22 -0500

    created console log for variable fullName

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,13 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = preston;
+let firstName = 'preston';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName = thomas;
+let lastName = 'thomas'
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = 'firstName' + 'lastName'
+let fullName = firstName + ' ' + lastName;
 // 4 - Console log the value of `fullName`
-
+console.log( 'My full name is', fullName );
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 
 // 6 - Console log this sentence, adding in the variables you created above:
