0c408dd107359f5521570d5fcd68257b640c6776
pthomas0309 <preston.thomas355@gmail.com>
2021-04-10 18:44:52 -0500

    added gambling conditional based on luckyNumber and adventurous

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -36,7 +36,12 @@ else {
 } // end not adventurous
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if ( luckyNumber == 2 && adventurous ) {
+  console.log( 'Roll the dice!' );
+}
+else {
+  console.log( 'Know when to hold em, know when to fold em' );
+}
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
