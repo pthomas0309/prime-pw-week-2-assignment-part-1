3999860b4d123614d2d1a3e67f658965a8859ed2
pthomas0309 <preston.thomas355@gmail.com>
2021-10-26 16:47:30 -0500

    fix ternary operator on last question

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -84,4 +84,4 @@ switch ( mostPets ) {
 
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-adventurous = false ? console.log( 'Adventures are great!' ) : console.log( 'How about we stay home?' )
+adventurous != false ? console.log( 'Adventures are great!' ) : console.log( 'How about we stay home?' )
